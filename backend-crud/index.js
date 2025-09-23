55e44875f8d5636e7aa9c26c6ce597bd815e4938
webxrahat <islammdrahat308@gmail.com>
2025-09-23 09:26:40 +0600

    setup

---
+++ backend-crud/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`This server is runing on ${port} port`);
+});
